ac36ffe386ab76cec37817dadccfc7b7b24eb64c
sharib <sharib.college@gmail.com>
2024-09-05 21:12:53 +0530

    Update Cart.js
    background

--- components/Cart.js
+++ components/Cart.js
@@ -14,13 +14,14 @@ function Cart() {
         <ul className="list-group">
           {cart.map((item, index) => (
             <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+              <img src={item.image} alt={item.name} style={{ width: '50px', height: '50px', objectFit: 'cover', marginRight: '10px' }} />
               {item.name} - ${item.price}
               <button className="btn btn-danger btn-sm" onClick={() => removeFromCart(index)}>Remove</button>
             </li>
           ))}
         </ul>
       )}
-      <Link to="/" className="btn btn-secondary mt-3">Back to Products</Link>
+      <Link to="/home" className="btn btn-secondary mt-3">Back to Products</Link>
     </div>
   );
 }
