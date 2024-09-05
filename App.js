fb49341d71899efdaaa47cba130b3fa84a03376b
sharib <sharib.college@gmail.com>
2024-09-05 21:11:19 +0530

    Update App.js
    background

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Cart from './components/Cart';
+import Home from './Components/Home';
+import Cart from './Components/Cart';
+import LandingPage from './Components/LandingPage'; // Import the LandingPage component
 import { CartProvider } from './context/CartContext';
+
+
 
 function App() {
   return (
+  <>
+
     <CartProvider>
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<LandingPage />} /> {/* Landing page route */}
+            <Route path="/home" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
       </Router>
     </CartProvider>
+    
+  </>
+    
+   
   );
 }
 
